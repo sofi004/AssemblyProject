e90424ce37d245c4b4e1bf30a61cb175ebb85c9a
joaotomas007 <joaotomasgomes@gmail.com>
2023-05-12 18:40:59 +0100

    init

--- Project.asm
+++ Project.asm
@@ -26,4 +26,45 @@ inicio:
      MOV  [APAGA_ECRÃ], R1	; apaga todos os pixels já desenhados (o valor de R1 não é relevante)
 	MOV	R1, 0			; cenário de fundo número 0
      MOV  [SELECIONA_CENARIO_FUNDO], R1	; seleciona o cenário de fundo
+
+
 ;###############################################################################
+DISPLAYS   EQU 0A000H  ; endereço dos displays de 7 segmentos (periférico POUT-1)
+TEC_LIN    EQU 0C000H  ; endereço das linhas do teclado (periférico POUT-2)
+TEC_COL    EQU 0E000H  ; endereço das colunas do teclado (periférico PIN)
+LINHA      EQU 1       ; linha a testar (4ª linha, 1000b)
+MASCARA    EQU 0FH     ; para isolar os 4 bits de menor peso, ao ler as colunas do teclado
+
+; **********************************************************************
+		
+; inicializações
+    MOV  R2, TEC_LIN   ; endereço do periférico das linhas
+    MOV  R3, TEC_COL   ; endereço do periférico das colunas
+    MOV  R4, DISPLAYS  ; endereço do periférico dos displays
+    MOV  R5, MASCARA   ; para isolar os 4 bits de menor peso, ao ler as colunas do teclado
+
+; corpo principal do programa
+ciclo:
+    MOV  R1, 0 
+    MOVB [R4], R1      ; escreve linha e coluna a zero nos displays
+
+espera_tecla:          ; neste ciclo espera-se até uma tecla ser premida
+    MOV  R1, LINHA     ; testar a linha 4 
+    MOVB [R2], R1      ; escrever no periférico de saída (linhas)
+    MOVB R0, [R3]      ; ler do periférico de entrada (colunas)
+    AND  R0, R5        ; elimina bits para além dos bits 0-3
+    CMP  R0, 0         ; há tecla premida?
+    JZ   espera_tecla  ; se nenhuma tecla premida, repete
+                       ; vai mostrar a linha e a coluna da tecla
+    SHL  R1, 4         ; coloca linha no nibble high
+    OR   R1, R0        ; junta coluna (nibble low)
+    MOVB [R4], R1      ; escreve linha e coluna nos displays
+    
+ha_tecla:              ; neste ciclo espera-se até NENHUMA tecla estar premida
+    MOV  R1, LINHA     ; testar a linha 4  (R1 tinha sido alterado)
+    MOVB [R2], R1      ; escrever no periférico de saída (linhas)
+    MOVB R0, [R3]      ; ler do periférico de entrada (colunas)
+    AND  R0, R5        ; elimina bits para além dos bits 0-3
+    CMP  R0, 0         ; há tecla premida?
+    JNZ  ha_tecla      ; se ainda houver uma tecla premida, espera até não haver
+    JMP  ciclo         ; repete ciclo
