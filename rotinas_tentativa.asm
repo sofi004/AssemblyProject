9c2881cde5f35ff6d292a7ca69b5ef6910908151
ist1106194 <sophiapiteira@gmail.com>
2023-05-20 00:34:55 +0100

    ee

--- rotinas_tentativa.asm
+++ rotinas_tentativa.asm
@@ -21,7 +21,7 @@ SELECIONA_CENARIO_FUNDO  EQU COMANDOS + 42H   ; endereço do comando para seleci
 SELECIONA_SOM_VIDEO  EQU COMANDOS + 48H   ; endereço do comando para selecionar uma video ou som
 REPRODUZ_SOM_VIDEO  EQU COMANDOS + 5AH   ; endereço do comando para iniciar a reprodução dum video ou som
 SUSPENDE_SOM_VIDEO  EQU COMANDOS + 5EH   ; endereço do comando para pausar video ou som
-CONTINUA_SOM_VIDEO  EQU COMANDOS + 64H   ; endereço do comando para continuar video ou som
+CONTINUA_SOM_VIDEO  EQU COMANDOS + 60H   ; endereço do comando para continuar video ou som
 TERMINA_SOM_VIDEO  EQU COMANDOS + 66H   ; endereço do comando para terminar a reprodução do som ou video
 
 COR_PIXEL_VERDE  EQU 0C0F0H   ; cor do pixel: verde em ARGB
