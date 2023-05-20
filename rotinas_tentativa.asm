6f5f36fc0c9c73728908ed25d85244c3797dc8d9
joaotomas007 <joaotomasgomes@gmail.com>
2023-05-20 11:13:42 +0100

    proj

--- rotinas_tentativa.asm
+++ rotinas_tentativa.asm
@@ -80,8 +80,8 @@ espera_tecla:   ; neste ciclo espera-se até uma tecla ser premida
     SHR  R1, 1   ; passa para a linha seguinte
     CMP  R1, 0   ; verifica se ja passamos pelas linhas todas
     JZ  restart_linhas ; voltamos ao inicio das linhas 
-    MOVB  [R2], R1   ; escrever no periférico de saída (linhas)
-    MOVB  R0, [R3]   ; ler do periférico de entrada (colunas)
+    MOVB [R2], R1   ; escrever no periférico de saída (linhas)
+    MOVB R0, [R3]   ; ler do periférico de entrada (colunas)
     MOV  R4, MASCARA   ; para isolar os 4 bits de menor peso, ao ler as colunas do teclado
     AND  R0, R4   ; elimina bits para além dos bits 0-3
     CMP  R0, 0   ; há tecla premida?
@@ -114,8 +114,8 @@ inicia_jogo_verificação:
 inicia_jogo:
     MOV  R0, 1   ; coloca 1 no registo para sabermos se o jogo está a correr ou não
     MOV  R5, 0   ; video número 0
-    MOV  [SELECIONA_SOM_VIDEO], R5   ; seleciona um video para cenário de fundo
-    MOV  [REPRODUZ_SOM_VIDEO], R5   ; inicia a reprodução do video de fundo do jogo
+    MOVB  [SELECIONA_SOM_VIDEO], R5   ; seleciona um video para cenário de fundo
+    MOVB  [REPRODUZ_SOM_VIDEO], R5   ; inicia a reprodução do video de fundo do jogo
     JMP  retorna_ciclo   ; depois de iniciar o jogo volta a restart linhas 
     
  fases_jogo:
@@ -130,25 +130,25 @@ suspende_jogo:
     CMP  R0, 2   ; o jogo já começou e está parado?
     JZ   continua_jogo   ;   prosseguir com o jogo
     MOV  R5, 0
-    MOV  [SUSPENDE_SOM_VIDEO], R5  ; pausa o video de fundo do jogo
+    MOVB [SUSPENDE_SOM_VIDEO], R5  ; pausa o video de fundo do jogo
     MOV  R0, 2   ; coloca o valor 2 no R0, simbolizando o facto de o jogo já ter começado, mas estar parado
     MOV  R5, 0
-    MOVB  [R3], R5
+    MOVB [R3], R5
     JMP  retorna_ciclo
 
 continua_jogo:
     MOV  R5, 0
-    MOV  [CONTINUA_SOM_VIDEO], R5  ; continua o video de fundo do jogo
+    MOVB [CONTINUA_SOM_VIDEO], R5  ; continua o video de fundo do jogo
     MOV  R0, 1   ; coloca novamente R0 a 1 uma vez que depois deste ciclo o jogo volta a correr
     MOV  R5, 0
-    MOVB  [R3], R5
+    MOVB [R3], R5
     JMP  retorna_ciclo
 
 termina_jogo:
     MOV  R5, 0   ; video número 0
-    MOV  [TERMINA_SOM_VIDEO], R5   ; pausa o video número 0
+    MOVB  [TERMINA_SOM_VIDEO], R5   ; pausa o video número 0
     MOV  R5, 1   ; cenário de fundo número 1
-    MOV  [SELECIONA_CENARIO_FUNDO], R5 ; seleciona o cenário de fundo
+    MOVB  [SELECIONA_CENARIO_FUNDO], R5 ; seleciona o cenário de fundo
     MOV  R0, 0   ; no caso em que o jogo foi terminado coloca-se R0 a 0, porque o jogo não está a correr
     JMP  retorna_ciclo
 
