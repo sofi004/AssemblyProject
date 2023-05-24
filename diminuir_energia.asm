b7acfc5bb0fbd74795d9f3259a41739f49ee95de
ist1106194 <sophiapiteira@gmail.com>
2023-05-24 17:41:44 +0100

    EE

--- diminuir_energia.asm
+++ diminuir_energia.asm
@@ -854,7 +854,7 @@ apaga_desenha_pixeis_sonda:
 mais_energia:
     PUSH R2
     MOV R2, [R4]
-    ADD R2, 0001H
+    ADD R2, 1
     MOV [R4], R2
     POP R2
     RET
@@ -862,7 +862,7 @@ mais_energia:
 menos_energia:
     PUSH R2
     MOV R2, [R4]
-    SUB R2, 0001H
+    SUB R2, 1
     MOV [R4], R2
     POP R2
     RET
